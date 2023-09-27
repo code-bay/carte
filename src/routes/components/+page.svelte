cda33e9518c3bee690a80e8ecfad97b6f384bbc2
HeronNepomuceno <heronnepo1@gmail.com>
2023-09-27 16:45:38 -0300

    feat: create route to tests components

---
+++ src/routes/components/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import SimpleButton from "$components/buttons/SimpleButton.svelte";
+	import FloatingActionButton from "$components/buttons/FloatingActionButton.svelte";
+</script>
+
+<div class="grid">
+	<div class="item">
+		<SimpleButton props={{
+			label: "Click me 1",
+			variant: "text"
+		}}/>
+
+		<SimpleButton props={{
+			label: "Click me 2",
+			variant: "text",
+			icon: "mdi:person-outline"
+		}}/>
+	</div>
+
+	<div class="item">
+		<FloatingActionButton props={{
+			icon: "mdi:heart-outline",
+			variant: "primary",
+			label: "Send",
+			size: "extended"
+		}}/>
+
+		<FloatingActionButton props={{
+			icon: "mdi:person-outline",
+			variant: "primary"
+		}}/>
+
+		<FloatingActionButton props={{
+			icon: "mdi:arrow-right",
+			variant: "primary",
+			size: "large"
+		}}/>
+	</div>
+</div>
+
+<style lang="scss">
+.grid {
+	align-self: center;
+	display: grid;
+	gap: 32px;
+	padding: 32px;
+	width: 100%;
+}
+
+.item {
+	display: flex;
+	gap: 16px;
+}
+</style>
