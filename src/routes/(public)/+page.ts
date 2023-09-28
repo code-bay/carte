5352f450c8a58dac9b593ce9c5233a872c45f51d
HeronNepomuceno <heronnepo1@gmail.com>
2023-09-28 16:24:50 -0300

    fix: wrong reference to types

--- src/routes/(public)/+page.ts
+++ src/routes/(public)/+page.ts
@@ -1,4 +1,4 @@
-import type { PageLoad } from '../$types';
+import type { PageLoad } from './$types';
 import { generateRandomColor } from '$lib/utils/generateRandomColor'
 
 export const load = (async () => {
