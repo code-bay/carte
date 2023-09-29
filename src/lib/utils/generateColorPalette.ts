495397e383c51fe96f4abf0c0d827dcf8013c4e2
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2023-09-29 10:48:01 -0300

    style: add editorconfig file

--- src/lib/utils/generateColorPalette.ts
+++ src/lib/utils/generateColorPalette.ts
@@ -1,18 +1,18 @@
 import { hexFromArgb, argbFromHex, themeFromSourceColor } from "@material/material-color-utilities";
 
 type Map = {
-  [key: string]: string | undefined
+	[key: string]: string | undefined
 }
 
 type Theme = "dark" | "light"
 
 export function generateColorPalette(color: string, variant: Theme = "dark") {
-  const theme = themeFromSourceColor(argbFromHex(color), []); 
-  const hexColors: Map = {};
+	const theme = themeFromSourceColor(argbFromHex(color), []); 
+	const hexColors: Map = {};
 
-  for (const [key, value] of Object.entries(theme.schemes[variant].toJSON())) {
-    hexColors[key] = hexFromArgb(value);
-  }
+	for (const [key, value] of Object.entries(theme.schemes[variant].toJSON())) {
+		hexColors[key] = hexFromArgb(value);
+	}
 
-  return hexColors;
+	return hexColors;
 }
