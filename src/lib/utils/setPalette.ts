3fb500501724b508ec4b015aeb526f04899cd7c5
HeronNepomuceno <heronnepo1@gmail.com>
2023-10-02 09:19:00 -0300

    feat: create a function to set all colors

---
+++ src/lib/utils/setPalette.ts
@@ -0,0 +1,20 @@
+export function setPalette(root: HTMLElement, palette: any) {
+	root.style.setProperty('--primary', palette.primary);
+	root.style.setProperty('--on-primary', palette.onPrimary);
+	root.style.setProperty('--primary-container', palette.primaryContainer);
+	root.style.setProperty('--on-primary-container', palette.onPrimaryContainer);
+	root.style.setProperty('--secondary-container', palette.secondaryContainer);
+	root.style.setProperty('--on-secondary-container', palette.onSecondaryContainer);
+	root.style.setProperty('--tertiary-container', palette.tertiaryContainer);
+	root.style.setProperty('--on-tertiary-container', palette.onTertiaryContainer);
+	root.style.setProperty('--surface', palette.surface);
+	root.style.setProperty('--on-surface', palette.onSurface);
+	root.style.setProperty('--surface-variant', palette.surfaceVariant);
+	root.style.setProperty('--on-surface-variant', palette.onSurfaceVariant);
+	root.style.setProperty('--outline', palette.outline);
+	root.style.setProperty('--outline-variant', palette.outlineVariant);
+	root.style.setProperty('--error', palette.error);
+	root.style.setProperty('--on-error', palette.onError);
+	root.style.setProperty('--inverse-surface', palette.inverseSurface);
+	root.style.setProperty('--inverse-on-surface', palette.inverseOnSurface);
+}
