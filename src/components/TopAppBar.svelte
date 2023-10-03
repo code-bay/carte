8fd3a67aca63540d5e062a35291826af11ac5073
HeronNepomuceno <heronnepo1@gmail.com>
2023-10-03 08:26:47 -0300

    refactor: use svg logo with dynamic recolor

--- src/components/TopAppBar.svelte
+++ src/components/TopAppBar.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
 	export let props: {
 		title: string,
-		logo?: string
 	}
 
-	const { title, logo } = props
+	const { title } = props
 </script>
 
 <header class="top-app-bar">
 	<section class="top-app-bar__section">
-		{#if logo}
-			<a class="logo" href="./">
-				<img class="top-app-bar__logo" src={logo} alt="app logo"/>
-			</a>
-		{/if}
-		<span class="top-app-bar__title" class:margin-left={logo}>{title}</span>
+		<a class="top-app-bar__logo" href="./">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 262 262">
+				<rect width="200" height="250" x="39.1" y="11.7" opacity=".5" rx="32"/>
+				<rect width="200" height="250" x="31" y="5.6" opacity=".8" rx="32" transform="rotate(-5 131 131)"/>
+			</svg>				
+		</a>
+		<span class="top-app-bar__title">{title}</span>
 	</section>
 	{#if $$slots}
 		<slot/>
@@ -36,16 +36,14 @@
 	}
 
 	&__logo {
+		fill: var(--primary);
 		width: 30px;
 	}
 
 	&__title {
 		color: var(--on-surface);
 		font: var(--title-large);
+		margin-left: 16px;
 	}
-}
-
-.margin-left {
-	margin-left: 16px;
 }
 </style>
