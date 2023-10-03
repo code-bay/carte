f5ecccbd335c987f4bad316991b121b7230e2697
HeronNepomuceno <heronnepo1@gmail.com>
2023-10-03 10:44:47 -0300

    refactor: insert limitations on width

--- src/components/sheets/BottomSheet.svelte
+++ src/components/sheets/BottomSheet.svelte
@@ -1,4 +1,8 @@
-<div class="bottom-sheet">
+<script lang="ts">
+	import { innerWidth } from "$lib/stores/innerWidth";
+</script>
+
+<div class="bottom-sheet" class:isOpen={$innerWidth < 1080}>
 	<slot />
 </div>
 
@@ -6,16 +10,18 @@
 .bottom-sheet {
 	background: var(--outline-variant);
 	border-radius: var(--corner-extra-large-top);
-	bottom: 0;
-	max-height: 480px;
+	max-width: 528px;
+	min-width: 360px;
 	overflow-y: auto;
 	padding: 32px 16px 16px 16px;
 	position: fixed;
 	transform: translateY(480px);
 	width: 100%;
+	z-index: 3;
 }
 
-.show {
+.isOpen {
+	bottom: 0;
 	transform: translateY(0);
 }
 </style>
