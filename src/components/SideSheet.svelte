85b825d9e4d75e9fced1e0b1534594119ceb3557
HeronNepomuceno <heronnepo1@gmail.com>
2023-10-04 20:19:11 -0300

    refactor: update side sheet to supports modal and standard mode

--- src/components/SideSheet.svelte
+++ src/components/SideSheet.svelte
@@ -1,54 +1,119 @@
 <script lang="ts">
-	export let props: {
-		title: string
-	}
+	import { innerWidth } from "$lib/stores/innerWidth";
+	import { isVisible } from "$lib/stores/isVisible";
+	import IconButton from "./buttons/IconButton.svelte";
+	import Divider from "./Divider.svelte";
 
+	export let props: { title: string }
 	const { title } = props
 </script>
 
-<div class="side-sheet">
+{#if $innerWidth < 840}
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div 
+		class="overlay" 
+		class:hiddenOverlay={$innerWidth < 840 && $isVisible === false}
+		on:click={() => $isVisible = false}
+	></div>
+{/if}
+
+<div 
+	class="side-sheet" 
+	class:isDesktop={$innerWidth >= 840} 
+	class:hiddenSheet={ $innerWidth < 840 && $isVisible === false}
+>
 	<div class="head">
 		<div class="title">{title}</div>
+
+		{#if $innerWidth < 840}
+			<IconButton 
+			onClick={() => $isVisible = false}
+			props={{
+				icon: "mdi:close",
+				variant: "standard"
+			}}/>
+		{/if}
 	</div>
-	<slot/>
+
+	<div class="body">
+		<slot name="body"/>
+	</div>
+
+	<Divider/>
+	<div class="footer">
+		<slot name="footer"/>
+	</div>
 </div>
 
 <style lang="scss">
 .side-sheet {
-	background: var(--outline-variant);
+	background: var(--surface);
 	border-radius: 16px 0 0 16px;
+	display: flex;
+	flex-direction: column;
 	height: 100%;
-	width: 292px;
+	max-width: 400px;
+	min-width: 292px;
+	width: clamp(292px, 70%, 400px);
 	overflow-y: auto;
-	padding: 16px 12px;
+	padding: 24px;
 	position: fixed;
 	top: 0;
-	right: -292px;
-	// transform: translateX(292px);
+	right: 0;
 	z-index: 3;
 	transition: transform .3s;
-
-	@media (min-width: 1080px) {
-		right: 0;
-	}
-
-	&.isOpen {
-		transform: translateX(-292px);
-
-		@media (min-width: 1080px) {
-			transform: translateX(0);
-		}
-	}
 }
 
 .head {
 	align-items: center;
 	display: flex;
 	justify-content: space-between;
+	margin-bottom: 16px;
+
+	.title {
+		font: var(--title-medium);
+		margin-right: 12px;
+	}
+}
+
+.body {
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	padding: 16px 0;
+	overflow-y: auto;
+}
+
+.footer {
+	align-items: center;
+	display: flex;
+	height: 72px;
+	padding-top: 16px;
+}
+
+.overlay {
+	background: rgba(0,0,0,0.5);
+	display: grid;
+	height: 100vh;
+	left: 0;
+	width: 100vw;
+	position: fixed;
+	top: 0;
+	z-index: 2;
+}
+
+.isDesktop {
+	border-radius: 0;
+	border-left: 1px solid var(--outline-variant);
 	width: 100%;
 }
 
-.title {
-	font: var(--title-medium);
+.hiddenOverlay {
+	display: none;
+}
+
+.hiddenSheet {
+	transform: translateX(400px);
 }
 </style>
